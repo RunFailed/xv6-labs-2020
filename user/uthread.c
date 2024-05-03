d5d7b5267743a85fa48eb46c6e81d03563ae58f6
zz <wuzz0179@gmail.com>
2024-05-03 16:54:46 +0800

    通过Lab7:1 Uthread: switching between threads测试

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,31 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+//Lab7:1 Uthread: switching between threads
+// Saved registers for kernel context switches.
+struct ucontext {
+    uint64 ra;
+    uint64 sp;
+
+    // callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+    struct ucontext ctx;     //Uthread 上下文，保存被调用者(callee)保存寄存器
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,6 +83,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+      thread_switch((uint64)(&t->ctx), (uint64)(&next_thread->ctx));
   } else
     next_thread = 0;
 }
@@ -77,6 +98,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  //确保当thread_schedule()第一次运行给定线程时，
+  // 该线程在自己的栈上执行传递给thread_create()的函数
+    t->ctx.ra = (uint64)func;
+    t->ctx.sp = (uint64)(t->stack + STACK_SIZE);
 }
 
 void 
